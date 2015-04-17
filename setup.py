3195f7c536c943090b03b260e803fbd01150b2dd
Patrick Gartung <gartung@fnal.gov>
2015-04-17 10:20:15 -0500

    go to 0.7.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 #     return git("describe --dirty")
 
 setup(name = 'lbne-build',
-      version = '0.7.4-dev',
+      version = '0.7.5',
       description = 'Worch/waf tools to build LBNE software.',
       author = 'Brett Viren',
       author_email = 'brett.viren@gmail.com',
